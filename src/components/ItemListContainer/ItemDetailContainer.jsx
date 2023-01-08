e96c8b3ada009ac68cd1be3b4b49a77f2004ef1f
Shibe <juligibelli@gmail.com>
2023-01-07 23:37:30 -0300

    se agrega rendering condicional para detailContainer

--- src/components/ItemListContainer/ItemDetailContainer.jsx
+++ src/components/ItemListContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useParams,Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getSingleItem } from "../../service/firebase";
 import Item from "./Item";
 import { cartContext } from "../../storage/cartContext";
@@ -10,9 +10,9 @@ import ItemCount from "./ItemCount";
 
 export default function ItemDetailContainer() {
   const [producto, setProducto] = useState({});
-  const [countInCart, setCountInCart] = useState(0)
+  const [countInCart, setCountInCart] = useState(0);
 
-  const {addToCart, removeItem} = useContext(cartContext)
+  const { addToCart, removeItem } = useContext(cartContext);
 
   const parametroURL = useParams().itemID;
   useEffect(() => {
@@ -25,26 +25,26 @@ export default function ItemDetailContainer() {
       });
   }, []);
 
-  function handleAddToCart(count){
-    setCountInCart(count)
-    addToCart(producto,count)
+  function handleAddToCart(count) {
+    setCountInCart(count);
+    addToCart(producto, count);
   }
 
   return (
     <div className="container py-6 is-flex-direction-column">
-     <div className="card-detail_img">
+      <div className="card-detail_img">
         <img src={producto.thumbnail} alt={producto.title} />
       </div>
       <div className="card-detail_detail">
         <h1>{producto.title}</h1>
         <h4 className="priceTag">$ {producto.price}</h4>
-        {producto.oferta && <h2 style={{color:"green"}}>{producto.oferta}% discount</h2>}
+        {producto.oferta && (<h2 style={{ color: "green" }}>{producto.oferta}% discount</h2> )}
         <p>{producto.description}</p>
       </div>
+      
+      {/* rendering condicional para mostrar o no itemcount y link a carrito*/}
 
-      <ItemCount onAddToCart={handleAddToCart}/>
-           
-      <Link to="/cart">Ir al carrito</Link>
+      {countInCart >0 ? <Link to="/cart" style={{display:"block",marginTop:"50px",color:"red"}}>Ir al carrito</Link> : <ItemCount onAddToCart={handleAddToCart} /> }
     </div>
   );
 }
